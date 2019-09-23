e69fa7a30dbdd93283ee4cbabe8fd3e08a34e22c
Genrev Arambulo <arambge@oocl.com>
2019-09-23 21:21:00 +0800

    Committed practice 7.
    Changes:
    1. Created a class (parent class) with a constructor and getters.
    2. Created another class which extends (Inheritance) another class (Parent class) and has an application of polymorphism using constructor.
    3. Created an object named Klass which was used as a property (Field) of class Student and Teacher.
    4. Uncommented the test file.

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,32 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        if (getKlass() != null)
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach " + getKlass().getDisplayName() +".";
+        else
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student student){
+        if (getKlass().equals(student.getKlass()))
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach " + student.getName() + ".";
+        else
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I don't teach " + student.getName() + "." ;
+    }
 }
