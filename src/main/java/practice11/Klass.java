92738141346ccaf9151449a706799846fc6b6a8d
Genrev Arambulo <arambge@oocl.com>
2019-09-23 21:34:44 +0800

    Committed practice 11.
    Changes:
    1. Created a class (parent class) with a constructor and getters.
    2. Created another class which extends (Inheritance) another class (Parent class) and has an application of polymorphism using constructor.
    3. Created an object named Klass which was used as a property (Field) of class Student and Teacher.
    4. Added a method in Klass named assignLeader and getLeader
    5. Added a validation that checks if both objects has same id, then both objects must be the same (even though they are initiated separately)
    6. Created a linkedlist for the class Teacher which was used as its property (Field) to accept one or many class of type Klass
    7. Added a validation if the property klass of Teacher is null
    8. Added implementation of Observable and observer.
    9. Uncommented the test file.

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,65 @@
 package practice11;
 
-public class Klass {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Klass extends Observable {
+    private int number;
+    private Student leader;
+    private List<Student> members;
+    private ArrayList<Observer> observerList;
+
+    public Klass(int number){
+        this.number = number;
+        members = new ArrayList<Student>();
+        observerList = new ArrayList<Observer>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + Integer.toString(getNumber());
+    }
+
+    public void assignLeader(Student student){
+        members.forEach(member -> {
+            if (member == student) {
+                leader = member;
+                notifyObservers(2);
+            }
+        });
+        if (leader == null)
+            System.out.print("It is not one of us.\n");
+    }
+
+    public void appendMember(Student student){
+        members.add(student);
+        notifyObservers(1);
+
+    }
+
+    public Student getLeader(){
+        return leader;
+    }
+
+    public List<Student> getMembers(){
+        return members;
+    }
+
+    @Override
+    public void addObserver(Observer o) {
+//        super.addObserver(o);
+        observerList.add(o);
+    }
+
+    @Override
+    public void notifyObservers(Object arg) {
+        observerList.forEach(observer -> {
+            observer.update(this, arg);
+        });
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,127 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Teacher extends Person implements Observer {
+    private Klass klass;
+    private LinkedList<Klass> classList;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass){
+        super(id,name,age);
+        this.klass = klass;
+        klass.addObserver(this);
+    }
+    public Teacher(int id, String name, int age, LinkedList<Klass> classList){
+        super(id,name,age);
+        this.classList = classList;
+        classList.forEach(klass1 -> klass1.addObserver(this));
+    }
+
+    @Override
+    public String introduce(){
+        if (getClasses() != null) {
+            StringBuilder classListString = new StringBuilder("");
+            classList.forEach(klass -> {
+                if (classListString.length() == 0)
+                    classListString.append(klass.getNumber());
+                else
+                    classListString.append(", " + klass.getNumber());
+            });
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " + classListString + ".";
+        } else
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student student){
+        if (isTeaching(student))
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach " + student.getName() + ".";
+        else
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I don't teach " + student.getName() + "." ;
+    }
+
+    public LinkedList<Klass> getClasses(){
+        return classList;
+    }
+
+    public boolean isTeaching(Student student){
+        return classList.contains(student.getKlass());
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        int indicator = (int) arg;
+        Klass klass = (Klass) o;
+        Student leader = klass.getLeader();
+
+        if (klass == this.klass || classList.contains(klass)){
+            if (indicator == 1){
+                klass.getMembers().forEach(student -> {
+                    System.out.print("I am " + getName() + ". I know " + student.getName() + " has joined " + klass.getDisplayName() + ".\n");
+                });
+            } else if(indicator == 2){
+                System.out.print("I am " + getName() + ". I know " + leader.getName() + " become Leader of " + klass.getDisplayName() + ".\n");
+            }
+        }
+    }
+
+//        if (arg instanceof Klass){
+//            Klass klass = (Klass) arg;
+//            if (this.)
+//            klass.getMembers().forEach(student -> {
+//                getClasses().forEach(klass1 -> {
+//                    if (student.getKlass() == klass1)
+//                        System.out.print("I am " + getName() + ". I know " + student.getName() + " has joined " + klass1.getDisplayName() + ".\n");
+//                    else if(student.getKlass() == this.klass)
+//                        System.out.print("I am " + getName() + ". I know " + student.getName() + " has joined " + klass.getDisplayName() + ".\n");
+//                });
+//            });
+//        } else if(arg instanceof Student){
+//            Student leader = (Student) arg;
+//            if (leader.getKlass() == this.klass)
+//                System.out.print("I am " + getName() + ". I know " + leader.getName() + " become Leader of " + klass.getDisplayName() + ".\n");
+//            else{
+//                classList.forEach(klass1 -> {
+//                    if (klass1 == leader.getKlass())
+//                        System.out.print("I am " + getName() + ". I know " + leader.getName() + " become Leader of " + klass1.getDisplayName() + ".\n");
+//                });
+//            }
+//        }
+//    }
+
+//    @Override
+//    public void update(Klass klass, Indicator indicator) {
+//        Student leader = klass.getLeader();
+//        if (indicator.equals(Indicator.MEMBER)){
+//            klass.getMembers().forEach(student -> {
+//                getClasses().forEach(klass1 -> {
+//                    if (student.getKlass() == klass1)
+//                        System.out.print("I am " + getName() + ". I know " + student.getName() + " has joined " + klass1.getDisplayName() + ".\n");
+//                    else if(student.getKlass() == this.klass)
+//                        System.out.print("I am " + getName() + ". I know " + student.getName() + " has joined " + klass.getDisplayName() + ".\n");
+//                });
+//            });
+//        } else if(indicator.equals(Indicator.LEADER)){
+//            if (leader.getKlass() == this.klass)
+//                System.out.print("I am " + getName() + ". I know " + leader.getName() + " become Leader of " + klass.getDisplayName() + ".\n");
+//            else{
+//                classList.forEach(klass1 -> {
+//                    if (klass1 == leader.getKlass())
+//                        System.out.print("I am " + getName() + ". I know " + leader.getName() + " become Leader of " + klass1.getDisplayName() + ".\n");
+//                });
+//            }
+//        }
+//    }
+
+//
+
 }
