7ced87860b7487bf82ba3d00948ed1fad2050383
Genrev Arambulo <arambge@oocl.com>
2019-09-23 21:18:44 +0800

    Committed practice 5.
    Changes:
    1. Created a class (parent class) with a constructor and getters.
    2. Created another class which extends (Inheritance) another class (Parent class) and has an application of polymorphism using constructor.
    3. Created an object named Klass which was used as a property (Field) of class Student and Teacher.
    4. Uncommented the test file.

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,22 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am at " + getKlass().getDisplayName() +".";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
