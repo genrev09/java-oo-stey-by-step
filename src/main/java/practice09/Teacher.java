b621b81eabe30d37d89659bb1ca5b048c980168f
Genrev Arambulo <arambge@oocl.com>
2019-09-23 21:29:10 +0800

    Committed practice 9.
    Changes:
    1. Created a class (parent class) with a constructor and getters.
    2. Created another class which extends (Inheritance) another class (Parent class) and has an application of polymorphism using constructor.
    3. Created an object named Klass which was used as a property (Field) of class Student and Teacher.
    4. Added a method in Klass named assignLeader and getLeader
    5. Added a validation that checks if both objects has same id, then both objects must be the same (even though they are initiated separately)
    6. Created a linkedlist for the class Teacher which was used as its property (Field) to accept one or many class of type Klass
    7. Uncommented the test file.

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,32 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends  Person{
+    private Klass klass;
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass){
+        super(id,name,age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        if (getKlass() != null)
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach " + getKlass().getDisplayName() +".";
+        else
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student student){
+        if (getKlass().equals(student.getKlass()))
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach " + student.getName() + ".";
+        else
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I don't teach " + student.getName() + "." ;
+    }
 }
